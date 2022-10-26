435df668d9c5b8de2d58accc90888d6e0eeec190
OleJake <jacobshawn8@outlook.com>
2022-10-26 04:39:23 -0400

    add code for local storage
    add code for local storage

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ let myLeads = []
 const inputBtn = document.getElementById("input-btn")
 let inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+let leadsfromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(document.getElementById("input-el").value)
     inputEl.value = ""
     renderLeads()
-
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
 })
 
 function renderLeads() {
